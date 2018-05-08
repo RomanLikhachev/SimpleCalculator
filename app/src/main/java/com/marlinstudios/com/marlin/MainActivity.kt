11e88317563cd59b64f33fff9435420347fd2004
RomanLikhachev <romkin63@gmail.com>
2018-05-08 21:19:11 +0400

    Added animation buttons + cls

--- app/src/main/java/com/marlinstudios/com/marlin/MainActivity.kt
+++ app/src/main/java/com/marlinstudios/com/marlin/MainActivity.kt
@@ -9,11 +9,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val stockText = answerString.text
         button_reset.setOnClickListener { v ->
-             answerString.animate().setDuration(100).rotationBy(360f)
-                     .rotationXBy(360f).rotationYBy(360F)
-            button_reset.animate().setDuration(100).rotationBy(-360f)
-                     .rotationX(-360f).rotationY(-360F)
-         }
+            answerString.animate().setDuration(500).rotationBy(360f)
+                answerString.text = stockText
+            button_reset.animate().setDuration(500).rotationBy(360f)
+            button1.animate().setDuration(500).rotationBy(360f)
+            button2.animate().setDuration(500).rotationBy(360f)
+            button3.animate().setDuration(500).rotationBy(360f)
+            button4.animate().setDuration(500).rotationBy(360f)
+            button5.animate().setDuration(500).rotationBy(360f)
+            button6.animate().setDuration(500).rotationBy(360f)
+            button7.animate().setDuration(500).rotationBy(360f)
+            button8.animate().setDuration(500).rotationBy(360f)
+            button9.animate().setDuration(500).rotationBy(360f)
+            buttonOneZero.animate().setDuration(500).rotationBy(360f)
+            buttonTwoZero.animate().setDuration(500).rotationBy(360f)
+            buttontThreeZero.animate().setDuration(500).rotationBy(360f)
+            button_ans.animate().setDuration(500).rotationBy(360f)
+            button_div.animate().setDuration(500).rotationBy(360f)
+            button_minus.animate().setDuration(500).rotationBy(360f)
+            button_plus.animate().setDuration(500).rotationBy(360f)
+            button_sqr.animate().setDuration(500).rotationBy(360f)
+            button_sqrt.animate().setDuration(500).rotationBy(360f)
+        }
     }
 }
